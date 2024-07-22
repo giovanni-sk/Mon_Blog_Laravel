97de213a946bee3d319b04071fa021b8ef5e520c
Giovanni <giovanni.sokenou2105@gmail.com>
2024-07-22 11:23:50 +0200

    @yield @extends @section

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Mon blog Laravel</title>
+</head>
 
 <body>
     <h1>Mon Blog Laravel</h1>
-
     <p>
         <a href="/contact-us">Contactez-nous</a>
     </p>
+    <!-- Le contenues de toutes les pages ici -->
+    @yield('contenu')
+    <!-- Le contenues de toutes les pages ici -->
 </body>
+
+</html>
