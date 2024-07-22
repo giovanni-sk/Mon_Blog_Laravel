2f07a7497eea159a1ce3e0c8ad4390da351f7621
Giovanni <giovanni.sokenou2105@gmail.com>
2024-07-22 13:34:53 +0200

    @if @else

--- resources/views/layouts/articles.blade.php
+++ resources/views/layouts/articles.blade.php
@@ -4,10 +4,14 @@ Articles
 @endsection
 @section('contenu')
     <h2>Articles</h2>
+@if ($articles)
 @foreach($articles as $article)
 <article>
     <h2>{{ $article["title"]}}</h2>
     <p>{{$article["body"]}}</p>
 </article>
 @endforeach
+@else
+<p>Ooopsss!!! 😭😢😭😢Aucun article trouvé.</p>
+@endif
 @endsection
